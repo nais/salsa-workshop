5c6ae178a88898bde2b52e292d79cdb9aaeb724a
J-K. Solbakken <jksolbakken@gmail.com>
2023-02-22 10:08:04 +0100

    liveness probes for k8s deployment

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,15 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	})
+
+	http.HandleFunc("/liveness", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "ready")
+	})
+
+	http.HandleFunc("/readiness", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "ready")
+	})
+
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
